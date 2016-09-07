a9daee181ea4986b09c7422ebe5846739c37a382
Asmir Mustafic <goetas@gmail.com>
2016-09-07 19:19:20 +0300

    do not use common trait anymore

--- src/Server.php
+++ src/Server.php
@@ -15,7 +15,6 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class Server
 {
-    use SoapCommon\ResultWrapperTrait;
     /**
      * @var Serializer
      */
@@ -90,9 +89,9 @@ class Server
             // @todo $fault->setActor() allow to set the current server actor
         }
         if (isset($fault)) {
-            $wrappedResult = $this->wrapResult($this->serializer, $fault, SoapCommon\SoapEnvelope\Messages\Fault::class);
+            $wrappedResult = $this->wrapResult($fault, SoapCommon\SoapEnvelope\Messages\Fault::class);
         } else {
-            $wrappedResult = $this->wrapResult($this->serializer, $result, $soapOperation['output']['message_fqcn']);
+            $wrappedResult = $this->wrapResult($result, $soapOperation['output']['message_fqcn']);
         }
 
         return $this->reply($wrappedResult);
@@ -134,7 +133,59 @@ class Server
         throw new ServerException("Can not find an operation to run $action service call");
     }
 
+    /**
+     * @param object|null $input
+     * @param string $class
+     * @return object
+     * @throws \Exception
+     */
+    private function wrapResult($input, $class)
+    {
+        if (!$input instanceof $class) {
+            $instantiator = new Instantiator();
+            $factory = $this->serializer->getMetadataFactory();
+            $previous = null;
+            $previousProperty = null;
+            $nextClass = $class;
+            $originalInput = $input;
+            $i = 0;
+            while ($i++ < 4) {
+                /**
+                 * @var $classMetadata ClassMetadata
+                 */
+                if ($previousProperty && in_array($nextClass, ['double', 'string', 'float', 'integer', 'boolean'])) {
+                    $previousProperty->setValue($previous, $originalInput);
+                    break;
+                }
+                $classMetadata = $factory->getMetadataForClass($nextClass);
+                if ($input === null && !$classMetadata->propertyMetadata) {
+                    return $instantiator->instantiate($classMetadata->name);
+                } elseif (!$classMetadata->propertyMetadata) {
+                    throw new \Exception("Can not determine how to associate the message");
+                }
+                $instance = $instantiator->instantiate($classMetadata->name);
+                /**
+                 * @var $propertyMetadata PropertyMetadata
+                 */
+                $propertyMetadata = reset($classMetadata->propertyMetadata);
 
+                if ($previous) {
+                    $previousProperty->setValue($previous, $instance);
+                } else {
+                    $input = $instance;
+                }
+                if ($originalInput instanceof $propertyMetadata->type['name']) {
+                    $propertyMetadata->setValue($instance, $originalInput);
+                    break;
+                }
+                $previous = $instance;
+                $nextClass = $propertyMetadata->type['name'];
+                $previousProperty = $propertyMetadata;
+            }
+        }
+
+        return $input;
+    }
 
     private function getXmlNamesDescription($object)
     {
